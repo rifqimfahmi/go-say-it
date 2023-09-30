860a616ecd255ccec92e1ec2b2351d0784f3593e
rifqimfahmi <rifqi416@gmail.com>
2023-10-01 00:00:18 +0800

    gpt feedback

--- inputparser/parser.go
+++ inputparser/parser.go
@@ -10,15 +10,13 @@ type ParserImpl struct {
 }
 
 func (i ParserImpl) parseArg(args []string) (*Input, error) {
-	input := &Input{}
 	if len(args) <= 1 {
-		return nil, fmt.Errorf("Arg length is <= 1")
+		return nil, fmt.Errorf("arg length is <= 1")
 	}
-	commandArgs := args[1:(len(args) - 1)]
-	wordsToSay := make([]string, len(commandArgs))
-	for _, arg := range commandArgs {
+	var wordsToSay []string
+	for _, arg := range args[1:(len(args) - 1)] {
 		wordsToSay = append(wordsToSay, arg)
 	}
-	input.word = wordsToSay
+	input := &Input{word: wordsToSay}
 	return input, nil
 }
